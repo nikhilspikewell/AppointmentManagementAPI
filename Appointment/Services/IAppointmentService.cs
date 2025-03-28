af9a114d8354fd128246f497677078ede70cdab0
nikhilspikewell <nikhil@spikewell.com>
2025-03-28 16:08:19 +0530

    Updated

--- Appointment/Services/IAppointmentService.cs
+++ Appointment/Services/IAppointmentService.cs
@@ -1,28 +1,4 @@
-﻿using AppointmentManagementAPI.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace AppointmentManagementAPI.Services
-{
-    public interface IAppointmentService
-    {
-        Task<IEnumerable<AppointmentDTO>> GetAppointmentsAsync();
-        Task<AppointmentDTO?> GetAppointmentByIdAsync(int id);
-        Task<IEnumerable<AppointmentDTO>> GetAppointmentsByNameAsync(string name);
-        Task<IEnumerable<AppointmentDTO>> GetAppointmentsByDateAsync(DateTime date);
-        Task AddAppointmentAsync(AppointmentDTO appointmentDto);
-        Task UpdateAppointmentAsync(int id, AppointmentDTO appointmentDto);
-        Task<bool> CancelAppointmentAsync(int id);
-        Task DeleteAppointmentAsync(int id);
-    }
-}
-
-
-
-
-
-//using AppointmentManagementAPI.DTOs;
+﻿//using AppointmentManagementAPI.DTOs;
 //using System;
 //using System.Collections.Generic;
 //using System.Threading.Tasks;
@@ -33,13 +9,37 @@ namespace AppointmentManagementAPI.Services
 //    {
 //        Task<IEnumerable<AppointmentDTO>> GetAppointmentsAsync();
 //        Task<AppointmentDTO?> GetAppointmentByIdAsync(int id);
-//        Task AddAppointmentAsync(AppointmentDTO appointmentDto);
-//        Task UpdateAppointmentAsync(int id, AppointmentDTO appointmentDto);
-
-//        // 🔹 Add missing methods
 //        Task<IEnumerable<AppointmentDTO>> GetAppointmentsByNameAsync(string name);
 //        Task<IEnumerable<AppointmentDTO>> GetAppointmentsByDateAsync(DateTime date);
+//        Task AddAppointmentAsync(AppointmentDTO appointmentDto);
+//        Task UpdateAppointmentAsync(int id, AppointmentDTO appointmentDto);
 //        Task<bool> CancelAppointmentAsync(int id);
-//        Task<bool> DeleteAppointmentAsync(int id);
+//        Task DeleteAppointmentAsync(int id);
 //    }
 //}
+
+
+
+
+
+using AppointmentManagementAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppointmentManagementAPI.Services
+{
+    public interface IAppointmentService
+    {
+        Task<IEnumerable<AppointmentDTO>> GetAppointmentsAsync();
+        Task<AppointmentDTO?> GetAppointmentByIdAsync(int id);
+        Task AddAppointmentAsync(AppointmentDTO appointmentDto);
+        Task UpdateAppointmentAsync(int id, AppointmentDTO appointmentDto);
+
+        // 🔹 Add missing methods
+        Task<IEnumerable<AppointmentDTO>> GetAppointmentsByNameAsync(string name);
+        Task<IEnumerable<AppointmentDTO>> GetAppointmentsByDateAsync(DateTime date);
+        Task<bool> CancelAppointmentAsync(int id);
+        Task<bool> DeleteAppointmentAsync(int id);
+    }
+}
